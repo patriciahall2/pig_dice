1c7376f41185adab2fdea237be2929875c0aae0c
Hailey Murto, Matt Phillips and Patricia Hall <thevetab@gmail.com>
2016-03-28 19:33:04 -0700

    make a random number generator

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function Roll = (number, total)
+  this.number = number;
+  this.total = total;
+
+var Rollbalance = "0"
+var Totalbalance = "0"
+
+var dieRoll = Math.floor(Math.random() * 6) +1;
+alert("You rolled a " + dieRoll);
+
+var score = function(dieRoll){
+  if (dieRoll === 1)   {
+  } Rollbalance === 0;
+} if (2 <= dieRoll) {
+ Rollbalance += dieRoll;
+}
+
+return Rollbalance
+
+$(document).ready(function() {
+  $("form#pingpong_form").submit(function(event) {
+
+  });
+});
