449d5686738a29fb08d6ce45313931bffc72d7f3
Hailey Murto, Matt Phillips and Patricia Hall <thevetab@gmail.com>
2016-03-28 20:42:53 -0700

    add a prototype framework

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
-function Roll = (number, total)
+function Roll(number, total) {
   this.number = number;
   this.total = total;
-
-var Rollbalance = "0"
-var Totalbalance = "0"
-
-var dieRoll = Math.floor(Math.random() * 6) +1;
-alert("You rolled a " + dieRoll);
-
-var score = function(dieRoll){
-  if (dieRoll === 1)   {
-  } Rollbalance === 0;
-} if (2 <= dieRoll) {
- Rollbalance += dieRoll;
 }
 
-return Rollbalance
+var Rollbalance = 0
+var Totalbalance = 0
+
+var dieRoll = Math.floor(Math.random() * 6) +1;
+  alert("You rolled a " + dieRoll);
+
+var score = function(dieRoll) {
+  if (dieRoll === 1)   {
+     Rollbalance === 0;
+  } if (2 <= dieRoll) {
+    Rollbalance += dieRoll;
+}
+};
+
+return Rollbalance;
+
 
 $(document).ready(function() {
-  $("form#pingpong_form").submit(function(event) {
+  $("#rollbutton").submit(function() {
+    event.preventDefault();
 
+    var Rollnumber = parseInt($("dieRoll").val());
+    $("#score").text(Rollbalance);
   });
 });
